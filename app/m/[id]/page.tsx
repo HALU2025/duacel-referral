e105af1c744ce4637cff9cef63cbbfc873d3e545
HALU2025 <masashi.suga@gmail.com>
2026-03-31 03:26:22 +0900

    FIX

--- app/m/[id]/page.tsx
+++ app/m/[id]/page.tsx
@@ -11,7 +11,7 @@ import {
   Settings, Mail, User, CheckCircle2, Ban, CheckCheck, ChevronRight, 
   Share, UserPlus, LayoutDashboard, Crown, Edit2, Loader2, Link as LinkIcon, 
   Trash2, Store, CreditCard, Send, LogOut, Info, ShoppingBag, BookOpen, 
-  Sparkles, PlayCircle, ShieldCheck, X
+  Sparkles, PlayCircle, ShieldCheck, X, Lock
 } from 'lucide-react'
 
 const getGradient = (name: string) => {
