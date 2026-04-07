9355f153febcc48fb9f2aee0fc7a4629a27d05a8
HALU2025 <masashi.suga@gmail.com>
2026-04-07 15:34:09 +0900

    FIX

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,15 +1,14 @@
-// app/manifest.ts
 import { MetadataRoute } from 'next'
-
+ 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Duacel',
-    short_name: 'Duacel',
-    description: '美容師紹介報酬管理システム',
-    start_url: '/',
-    display: 'standalone',
+    name: 'Duacel 紹介プログラム', // インストール時に表示される正式名称
+    short_name: 'Duacel', // ホーム画面のアイコン下に表示される短い名前
+    description: 'Duacelの紹介プログラム・メンバー用アプリです',
+    start_url: '/', // アイコンをタップした時に最初に開くページ
+    display: 'standalone', // ★ここが重要！URLバーを消して全画面アプリのように動かす設定
     background_color: '#ffffff',
-    theme_color: '#4338ca',
+    theme_color: '#111827', // ステータスバー（時計や電池マークの帯）の色
     icons: [
       {
         src: '/icon-192x192.png',
